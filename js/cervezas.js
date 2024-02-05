b3359f03e63ed9c47ee75a067cc596829cc11e57
juliobotta <jrb12494@gmail.com>
2024-02-05 20:38:32 -0300

    creadas páginas de categorias, falta pulir la lógica de javascript

---
+++ js/cervezas.js
@@ -0,0 +1,29 @@
+let arrayProductos = JSON.parse(localStorage.getItem("productos")) || [];
+
+let cuerpoCervezas = document.getElementById("contenedor-cervezas");
+
+function categoriaCervezas() {
+    const cervezas = arrayProductos.filter(
+        (elemento) => elemento.categoria === "cerveza"
+    );
+
+    cuerpoSugerencias.innerHTML = "";
+    arraySugerencias.forEach((elemento) => {
+        cuerpoCervezass.innerHTML += ` 
+            <div class="col justify-content-center my-3">
+                <div class="card my-2 mx-auto mx-sm-0" style="width: 16rem;">
+                    <img src="${elemento.urlImagen}" class="card-img-top tamaño-imagen-card" alt="${elemento.nombre}">
+                    <div class="card-body">
+                        <h5 class="card-title">${elemento.nombre}</h5>
+                        <p class="card-text">$ ${elemento.precio}</p>
+                        <div class="text-center">
+                            <a class="btn button-card btn-sm" href="/pages/descripcion_producto.html" onclick="verProducto(${elemento.id})">Ver producto</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    });
+}
+window.verProducto = function (idProducto) {
+    localStorage.setItem("idDetalleProducto", idProducto);
+}
---
+++ js/vinos.js
@@ -0,0 +1,29 @@
+let arrayProductos = JSON.parse(localStorage.getItem("productos")) || [];
+
+let cuerpoVinos = document.getElementById("contenedor-vinos");
+
+function categoriaVinos() {
+    const vinos = arrayProductos.filter(
+        (elemento) => elemento.categoria === "vino"
+    );
+
+    cuerpoSugerencias.innerHTML = "";
+    arraySugerencias.forEach((elemento) => {
+        cuerpoVinos.innerHTML += ` 
+            <div class="col justify-content-center my-3">
+                <div class="card my-2 mx-auto mx-sm-0" style="width: 16rem;">
+                    <img src="${elemento.urlImagen}" class="card-img-top tamaño-imagen-card" alt="${elemento.nombre}">
+                    <div class="card-body">
+                        <h5 class="card-title">${elemento.nombre}</h5>
+                        <p class="card-text">$ ${elemento.precio}</p>
+                        <div class="text-center">
+                            <a class="btn button-card btn-sm" href="/pages/descripcion_producto.html" onclick="verProducto(${elemento.id})">Ver producto</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    });
+}
+window.verProducto = function (idProducto) {
+    localStorage.setItem("idDetalleProducto", idProducto);
+}
